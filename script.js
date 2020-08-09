40a86b2da8074d81556c3638d197703476cef2f2
MohammadErfanGooneh <mohammad.erfan.gooneh@gmail.com>
2020-08-09 22:19:37 +0000

    random choices and detector created!

--- script.js
+++ script.js
@@ -7,11 +7,47 @@ const result_div = document.querySelector(".result") ;
 const rock_div = document.getElementById("r") ; 
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s")  ;
-document.getElementById("r").addEventListener('click' , function mamad(){
-    console.log("salam") ; 
-})
+function random(){
+    const choices=['p' , 'r' , 's'] ; 
+    const ranNum = Math.floor(Math.random()*3) ;
+    return  choices[ranNum] ; 
+}
+function game(userChoice){
+    const comChoice = random() ; 
+    switch(comChoice + userChoice){
+        case "rp" :
+        case "ps" :
+        case "sr" :
+            console.log("win!") ; 
+            break ; 
+        case "rs" :
+        case "sp" :
+        case "pr" :
+            console.log("lose!") ;
+            break ; 
+        case "rr" :
+        case "pp" :
+        case "ss" :
+            console.log("draw!") ; 
+            break ; 
+    }    
+}
+game() ; 
 
 
+function main(){
+    rock_div.addEventListener('click' , function(){
+        game("r") ; 
+    })
+    paper_div.addEventListener('click' , function(){
+        game("p") ;  
+    })
+    scissors_div.addEventListener('click' , function(){
+        game("s") ; 
+    })
+}
+
+main() ;
 
 
 
