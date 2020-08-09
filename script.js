b2b31cfaa1881cbe32a44d93b1bca15276d1df22
MohammadErfanGooneh <mohammad.erfan.gooneh@gmail.com>
2020-08-09 21:37:11 +0000

    fixing some problems and script tag moved

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const userScore = 0 ;
 const computerScore = 0 ; 
 const userScore_html = document.getElementById("user-score");
 const computerScore_html = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result") ; 
+const rock_div = document.getElementById("r") ; 
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s")  ;
+document.getElementById("r").addEventListener('click' , function mamad(){
+    console.log("salam") ; 
+})
+
+
+
+
+
+
+
+
+
+
+
