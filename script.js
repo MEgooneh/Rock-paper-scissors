0e35e75f7cf0d8cf3b6620f4552e5a0b3a6d5cc4
MohammadErfanGooneh <mohammad.erfan.gooneh@gmail.com>
2020-08-09 13:16:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const userScore = 0 ; 
+const computerScore = 0 ; 
+const userScore_html = document.getElementById("user-score");
+const computerScore_html = document.getElementById("computer-score");
